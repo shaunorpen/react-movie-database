b7044743db48c46aca51728f408b3722517d5414
Shaun Orpen <shaunorpen@Shauns-MacBook-Pro.local>
2020-01-03 21:31:37 +0000

    Grid and MovieThumb

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { API_URL, API_KEY, API_BASE_URL, POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 import HeroImage from './elements/HeroImage';
 import SearchBar from './elements/SearchBar';
@@ -7,15 +8,41 @@ import MovieThumb from './elements/MovieThumb';
 import LoadMoreBtn from './elements/LoadMoreBtn';
 import Spinner from './elements/Spinner';
 
-const Home = () => (
-    <>
-        <HeroImage />
-        <SearchBar />
-        <Grid />
-        <MovieThumb />
-        <Spinner />
-        <LoadMoreBtn />
-    </>
-)
+import { useHomeFetch } from './hooks/useHomeFetch';
+
+import NoImage from './images/no_image.jpg';
+
+const Home = () => {
+    const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+    const [searchTerm, setSearchTerm] = useState('');
+
+    if (error) return <div>Something went wrong!</div>
+    if (!state.movies[0]) return <Spinner />;
+
+    return (
+        <>
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+                title={state.heroImage.original_title}
+                text={state.heroImage.overview}
+            />
+            <SearchBar />
+            <Grid header={searchTerm ? 'Search Results' : 'Popular Movies'}>
+                {state.movies.map(movie => (
+                    <MovieThumb
+                        key={movie.id}
+                        clickable
+                        image={ movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}` : NoImage }
+                        movieId={movie.id}
+                        movieName={movie.original_title}
+                    />
+                ))}
+            </Grid>
+            <MovieThumb />
+            <Spinner />
+            <LoadMoreBtn />
+        </>
+    )  
+} 
 
 export default Home;
