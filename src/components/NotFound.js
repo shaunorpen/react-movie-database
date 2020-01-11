76cb6417bbc943e10098f8058fc37ef5d5c19913
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-01-11 21:36:38 +0000

    Add reach router

---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => (<div>Whoops! Nothing Found Here!</div>)
+
+export default NotFound;
--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from '@reach/router';
 
 import { StyledMovieThumb } from '../styles/StyledMovieThumb';
 
 const MovieThumb = ({ image, movieId, clickable }) => (
     <StyledMovieThumb>
         {
-            clickable ? <img className='clickable' src={image} alt='moviethumb' /> : <img src={image} alt='moviethumb' />
+            clickable 
+            ? <Link to={`/${movieId}`} >
+                <img className='clickable' src={image} alt='moviethumb' />
+              </Link>
+            : <img src={image} alt='moviethumb' />
         }
     </StyledMovieThumb>
 )
