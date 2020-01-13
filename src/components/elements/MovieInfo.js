0b128d6f91837c568fd32d25bbf4ab055d3d7225
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-01-13 21:14:59 +0000

    Update indent

--- src/components/elements/MovieInfo.js
+++ src/components/elements/MovieInfo.js
@@ -24,18 +24,18 @@ const MovieInfo = ({ movie }) => (
                 <h1>{ movie.title }</h1>
                 <h3>PLOT</h3>
                 <p>{ movie.overview }</p>
-            <div className="rating-director">
-                <div>
-                    <h3>IMDB RATING</h3>
-                    <div className="score">{ movie.vote_average }</div>
+                <div className="rating-director">
+                    <div>
+                        <h3>IMDB RATING</h3>
+                        <div className="score">{ movie.vote_average }</div>
+                    </div>
+                    <div className="director">
+                        <h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
+                        {movie.directors.map(element => (
+                            <p key={ element.credit_id }>{ element.name }</p>
+                        ))}
+                    </div>
                 </div>
-                <div className="director">
-                    <h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
-                    {movie.directors.map(element => (
-                        <p key={ element.credit_id }>{ element.name }</p>
-                    ))}
-                </div>
-            </div>
             </div>
         </div>
     </StyledMovieInfo>
